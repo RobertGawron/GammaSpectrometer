0627048ae842d3e70c2c4ecffbeaed37d92e4f9e
Robert Gawron <robert_gawron@yahoo.fr>
2020-07-11 15:02:01 +0200

    cleanup, choose correct values for simulation

--- Simulation/HVPowerSupplyElementValuesCalculator/HVPowerSupplyElementValuesCalculator.m
+++ Simulation/HVPowerSupplyElementValuesCalculator/HVPowerSupplyElementValuesCalculator.m
@@ -1,15 +1,7 @@
-############### Assumend constants ###############
+############### Calculate minimum coil inductance ###############
 
 # Input power supply
 Vin = 12;
-
-# Transformer ratio primary to secondary
-#Tratio = 6;
-
-# Transformer looses (I don't know how to calculate it)
-#Tlooses  = 0.1;
-
-############### Calculate minimum coil inductance ###############
 
 # Duty cycle of the switch
 D = 0.5;
@@ -18,27 +10,23 @@ D = 0.5;
 Iout = 20e-3;
 
 # Switching frequency
-Fs = 100e3;
+#Fs = 100e3;
 
 function Lmin = calculate_Lmin(Vin, Iout, D, Fs)
-    Lmin = D.*(1-D).*Vin ./ (2.*Iout .*Fs);
+    Lmin = (D .* (1-D) .* Vin) ./ (2 .* Iout .* Fs);
 end
 
 
-printf ("Lmin = %d\n", calculate_Lmin(Vin, Iout, D, Fs));
-
-# TODO this a try do a 2D graph in Octave
-frequency = (1e4: 1e4 : 1e6);
-
-response = arrayfun(@(f) calculate_Lmin(Vin, Iout, D, f), frequency);
-
-plot(frequency, response);
+frequency = (1e5: 1e4 : 1e6);
+Iout = (10e-3 : 5e-3 : 50e-3);
 
 
-# TODO this a try to create a 3d graph in Octave
-[X,Y] = meshgrid(frequency,  0.1 : 0.05 : 0.9);
-Z = calculate_Lmin(Vin, Iout, Y, X)
+[X,Y] = meshgrid(Iout, frequency);
+Z = calculate_Lmin(Vin, X, D, Y)
 surf(X,Y,Z)
+
+#printf ("Lmin = %d\n", calculate_Lmin(Vin, Iout, D, Fs));
+
 
 ############### Calculate feedback resistors ###################
 
